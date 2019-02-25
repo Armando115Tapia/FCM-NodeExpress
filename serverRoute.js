902283707e2e570dd4200d94910984eba9e53988
Armando Tapia <jusnfds123@hotmail.com>
2019-02-25 11:08:19 -0500

    Correct to serverRouter

--- serverRoute.js
+++ serverRoute.js
@@ -4,14 +4,19 @@
  */
 //var formidable = require('formidable');
 
-var FCM = require('fcm-node');
+//var FCM = require('fcm-node');
 var serverKey="AAAA9NlbMFc:APA91bEZeh9qxhkQLkHkpmWu0_eoLQgtHWYCZ_neUVmKI8zVVqTjqTi8_w8-EXoOgkt2xE_wK0EULiZz12HTo1ajfOioy5I-7KPFKv99Em3_0mZabvh1QJsbCkYxBMqd0Qjp3wAMpD9Z"; 
 //You will find this in your firebase console
-var fcm = new FCM(serverKey);
+//var fcm = new FCM(serverKey);
 
 //To get server key Select your project > Click the Gear icon(Settings) > Move to 'Cloud Messaging' tab .
 //Under Project credentials your will get your so called long 'Server Key'
  
+var fcm = require('fcm-notification');
+    var token = "eH1KXVCbdW4:APA91bFwMAl03JfrFFfVJ3v05wEKHqJxA_MkEYKegfB2LZaA6AEs5cUT7w_duwEMEjfhdrBNvy8YP0P-BTV-e-5f7xgGCi4FTGdjiKgH5Mhn2sde1GoWvvdEdiUS5Y2UHZXBq8c7Go97";
+    var FCM = new fcm('./limpizimo-4755e-firebase-adminsdk-itco5-8a0e83e7b1.json');
+
+
 module.exports=function(app){
  
  //PRIMERA OPCION USANDO
@@ -47,9 +52,6 @@ module.exports=function(app){
 //SEGUNDA OPCION
 //USADO: https://www.npmjs.com/package/fcm-notification
 app.get('/fcm-notification', function(req,res){
-    var fcm = require('fcm-notification');
-    var token = "fpyGWldaOeQ:APA91bF4AdueiScPY1oCjMWeaUBvO6SLM8Zl-A-ADNYxyGfmxcohvZlEQrHk_7NTTuRiD1KgT1Ae3WzHTt_5gm7WfVyf47Oj86DWn_UtotqC9J77T2OAUhoGunQ8mNDT78Ki4Y_0N7k5";
-    var FCM = new fcm('./limpizimo-4755e-firebase-adminsdk-itco5-8a0e83e7b1.json');
     var message = {
         data: {    //This is only optional, you can send any data
             score: '850',
@@ -57,7 +59,7 @@ app.get('/fcm-notification', function(req,res){
         },
         notification:{
             title : 'Limpizimo',
-            body : 'Cuerazo de la notificacion',
+            body : 'Tu servicio a sido confirmado, la empleada llegara',
             
         },
         token : token
